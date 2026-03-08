345cc3b84f220a6bfe90f1d6f6f564ce109a4869
ryusoh <zhuangliulz@gmail.com>
2026-03-08 01:24:16 -0800

    feat: make Anki UI elements transparent to allow glass effect to show through

--- animated_glass_background/__init__.py
+++ animated_glass_background/__init__.py
@@ -13,10 +13,11 @@ def on_webview_will_set_content(web_content: WebContent, context):
     if not context:
         return
         
-    # We apply this to the main user screens:
-    ctx_name = context.__class__.__name__
-    if ctx_name in ["DeckBrowser", "Overview", "Reviewer"]:
+    # Check if the webview is loading one of the main UI components
+    css_str = str(web_content.css)
+    if any(target in css_str for target in ["deckbrowser.css", "overview.css", "reviewer.css", "toolbar.css", "toolbar-bottom.css", "reviewer-bottom.css"]):
         addon_url = f"/_addons/{addon_dir_name}/web/glass_effect.js"
-        web_content.js.append(addon_url)
+        if addon_url not in web_content.js:
+            web_content.js.append(addon_url)
 
 webview_will_set_content.append(on_webview_will_set_content)
